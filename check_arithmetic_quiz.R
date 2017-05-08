cd17567d5c204cd37c47ffe07241c36756442143
Josh Goldstein <josh.goldstein@berkeley.edu>
2017-05-08 12:58:16 -0700

    added ignore to some files

---
+++ check_arithmetic_quiz.R
@@ -0,0 +1,10 @@
+.qnumber1.1 = 1.1
+.instructions1.1 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.correct.answer1.1 = "A"
+.hint1.1 = "Try '2+2' from R console"
+.explanation1.1 = "Hard to explain"
+.qnumber1.2 = 1.2
+.instructions1.2 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.correct.answer1.2 = "B"
+.hint1.2 = "Try 'Re(exp(pi * 1i))' from R console"
+.explanation1.2 = "exp(pi * i) equals cos(pi) + i * sin(pi)."
